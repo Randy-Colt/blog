54537830542665e21e1e2660c929617cc4626fff
Андрей Логвинов <andryu04101999@gmail.com>
2023-12-28 13:58:42 +0300

    url5

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -6,14 +6,18 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
+     path('',
+          views.PostListView.as_view(),
+          name='index'),
      path('category/<slug:category_slug>/',
           views.CategoryPostsView.as_view(),
           name='category_posts'),
      path('posts/<int:post_id>/',
           views.PostDetailView.as_view(),
           name='post_detail'),
-     path('', views.PostListView.as_view(), name='index'),
-     path('posts/create/', views.PostCreateView.as_view(), name='create_post'),
+     path('posts/create/',
+          views.PostCreateView.as_view(),
+          name='create_post'),
      path('posts/<int:post_id>/edit/',
           views.PostUpdateView.as_view(),
           name='edit_post'),
